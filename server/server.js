9816b1cf930fd6b12eee39071c2060e3ab01c605
shivani <shivaniw69@gmail.com>
2025-09-11 04:38:57 +0530

    index

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 
    const express=require("express")
    const app=express();
-   const bodyparser=require("body-parser")
-   const cors=require("cors")
+   const bodyparser=require("body-parser");
+   const cors=require("cors");
  
-   const mongoose=require("mongoose")
+   const mongoose=require("mongoose");
 
    //Body Parser
-   app.use(bodyparser.urlencoded({extended:true});
-   app.use(bodyparser.json());
+   app.use(bodyparser.urlencoded({extended:true}))
+   app.use(bodyparser.json())
    app.use(cors());
+   mongoose.connect(process.env.DBCON).then(()=>{
+      console.log("database connected succesfully!!")
+   })
 
   const Port=process.env.PORT ||8000
    app.listen(Port,()=>{
