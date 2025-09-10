564770f723f31d480c98ccc0b2a252292df153d8
shivani <shivaniw69@gmail.com>
2025-09-11 04:34:47 +0530

    index

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 
    const express=require("express")
    const app=express();
-   const body
+   const bodyparser=require("body-parser")
+   const cors=require("cors")
+ 
+   const mongoose=require("mongoose")
+
+   //Body Parser
+   app.use(bodyparser.urlencoded({extended:true});
+   app.use(bodyparser.json());
+   app.use(cors());
 
   const Port=process.env.PORT ||8000
    app.listen(Port,()=>{
     console.log(`Server run on port$(Port)`)
    })
-   const bodyparser=require("body-parser")
-   const cors=require("cors")
- 
-   const mongoose=require("mongoose")
+   
