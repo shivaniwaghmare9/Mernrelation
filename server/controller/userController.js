6e4c7242996958c5e7891bdc4c693aa98410bf8d
shivani <shivaniw69@gmail.com>
2025-09-11 05:00:40 +0530

    route

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -2,7 +2,22 @@
 const profileModel=require("../model/profileModel")
 const userModel=require("../model/userModel")
 
+const userSave=async()=>{
+    const {username,email,firstname,lastname}=req.body;
+    const User=await userModel.create({
+        username:username,
+        email:email
+    })
+    console.log(User)
+    const Profile=await profileModel.create({
+        firstname:firstname,
+        lastname:lastname,
+        userid:User._id
+    })
+    res.send("okkk!!")
+}
 
 module.exports={
-    
+     userSave
+
 }
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,4 +3,5 @@ const express=require("express")
 const route=express.Router();
 const userController=require("../controller/userController")
 
+route.get("/usersave",userController.userSave);
 module.exports=route;
