1072ca5bd327f3b4d966f2eecdf46b36f9775c94
shivani <shivaniw69@gmail.com>
2025-09-11 05:09:48 +0530

    top

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,24 @@
 
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Layout from "./Layout";
+import Home from "./Pages/Home";
+import Registration from "./Pages/Registration";
+import Display from "./Pages/Display";
+const App=()=>{
+   return(
+    <>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>}/>
+          <Route path="home" element={<Home/>}/>
+          <Route path="registration" element={<Registration/>}/>
+          <Route path="display" element={<Display/>}/>
+
+        </Route>
+    </Routes>
+    </BrowserRouter>
+    </>
+   )
+}
+export default App;
