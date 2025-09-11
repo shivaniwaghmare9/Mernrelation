1a5278ac6b1bd63d4d105ae8f7453a33f1874479
shivani <shivaniw69@gmail.com>
2025-09-11 08:52:10 +0530

    dsgf

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,6 +33,7 @@ import Layout from "./Layout";
 import Home from "./Pages/Home";
 import Registration from "./Pages/Registration";
 import Display from "./Pages/Display";
+import Addmore from "./Pages/Addmore";
 const App=()=>{
    return(
     <>
@@ -43,6 +44,7 @@ const App=()=>{
           <Route path="home" element={<Home/>}/>
           <Route path="registration" element={<Registration/>}/>
           <Route path="display" element={<Display/>}/>
+          <Route path="addmore/:id" element={<Addmore/>}/>
 
         </Route>
     </Routes>
