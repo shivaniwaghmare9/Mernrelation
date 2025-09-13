cb9c8d5712b83081a40d6a496e21926c94e1edfb
shivani <shivaniw69@gmail.com>
2025-09-13 23:39:13 +0530

    auther

--- server/routes/autherRoute.js
+++ server/routes/autherRoute.js
@@ -1,13 +1,3 @@
-// //========================================================ONE-TO-MANY RELATION=====================================================
-// const express=require("express")
-// const route=express.Router();
-// const autherController=require("../controller/autherController")
-
-// route.post("/authersave",autherController.addAuther);
-// route.get("/autherdisplay",autherController.autherDisplay);
-// route.post("/booksave",autherController.bookSave);
-
-// module.exports=route;
 //========================================================ONE-TO-MANY RELATION=====================================================
 const express=require("express")
 const route=express.Router();
