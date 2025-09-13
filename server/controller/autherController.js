99eaccd38585cab6b1656de62b6bf1651f40b03b
shivani <shivaniw69@gmail.com>
2025-09-13 23:41:23 +0530

    controller

--- server/controller/autherController.js
+++ server/controller/autherController.js
@@ -1,3 +1,44 @@
+// //========================================================ONE-TO-MANY RELATION=====================================================
+
+
+// const autherModel=require("../model/autherModel")
+// const bookModel=require("../model/bookModel")
+
+// const addAuther=async(req,res)=>{
+//      const {authername,email,bookname,price}=req.body;
+//     const book=await bookModel.create({
+//         bookname:bookname,
+//         price:price,
+       
+//     })
+//     // console.log(auther)
+//     const auther=await autherModel.create({
+//         authername:authername,
+//         email:email,
+//          //booksid:[]
+//     })
+//   await autherModel.findByIdAndUpdate(auther._id,{$push:{booksid:book._id}})
+//     res.send("data save!!")
+// }
+// const autherDisplay=async(req,res)=>{
+//       const auther= await autherModel.find().populate("booksid")
+//       res.send(auther);
+// }
+// const bookSave=async(req,res)=>{
+//      const {bookname,price,id}=req.body;
+//      const book=await bookModel.create({
+//         bookname:bookname,
+//         price:price
+//      })
+//      await autherModel.findByIdAndUpdate(id,{$push:{booksid:book._id}})
+//      res.send("book save")
+// }
+
+// module.exports={
+//     addAuther,
+//     autherDisplay,
+//     bookSave
+// }
 //========================================================ONE-TO-MANY RELATION=====================================================
 
 
