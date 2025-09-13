afb85360005385b307eadf4592245d4c91461a9f
shivani <shivaniw69@gmail.com>
2025-09-13 23:40:29 +0530

    route

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,12 +1,4 @@
 
-// const express=require("express")
-// const route=express.Router();
-// const userController=require("../controller/userController")
-
-// route.post("/save",userController.userSave);
-// route.get("/display",userController.userDisplay);
-// module.exports=route;
-
 const express=require("express")
 const route=express.Router();
 const userController=require("../controller/userController")
